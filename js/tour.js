4fc28cccfdb2a0fd00fb2c42ba772cca5025bcae
Fernando Saldaña <fer_ozz01@hotmail.com>
2015-10-16 13:15:14 -0500

    Update tour.js

--- js/tour.js
+++ js/tour.js
@@ -13,10 +13,42 @@ var tour = new Tour({
     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
     placement: "right"
   },
-
-
-
-
+  {
+    element: ".tour-002",
+    title: "Datos Generales",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    placement: "right"
+  },
+  {
+    element: ".tour-003",
+    title: "Ocular Navegación",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    placement: "bottom"
+  },
+  {
+    element: ".tour-004",
+    title: "Pagina de Inicio",
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    placement: "bottom"
+  },
+  {
+    element: ".tour-005",
+    title: "Menu Desplegable,
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    placement: "bottom"
+  },
+  {
+    element: ".tour-006",
+    title: "Información de la Pagina,
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    placement: "top"
+  },
+  {
+    element: ".tour-006",
+    title: "Contenido de la Pagina,
+    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    placement: "top"
+  },  
 ]});
 
 
